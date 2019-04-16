f07ca26cf3d4e00d61052614794aad27b497bb02
fahmisalman <fahmisalman@gmail.com>
2019-04-16 10:45:06 +0700

    Add joblib

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Model import Data_Proc
 from Model import Word2Vec
 from Model import SVM
+from sklearn.externals import joblib
 import numpy as np
 import csv
 import os
@@ -18,9 +19,13 @@ def import_csv(loc):
 if __name__ == '__main__':
 
     Data_Proc.data_process()
+
     x_train = np.array(Word2Vec.fit(import_csv(os.getcwd() + '/Model/Data/Data_train_preprocessing_k-5.csv')))
     y_train = np.array(import_csv(os.getcwd() + '/Model/Data/Target_train_k-5.csv'))
 
     y_train = np.int_(y_train)
-    SVM.fit(x_train, y_train[:, 0])
+
+    for i in range(3):
+        model = SVM.fit(x_train, y_train[:, i])
+        joblib.dump(model, os.getcwd() + '/Model/SVM_Model/Model_{}.pkl'.format(i + 1))
 
